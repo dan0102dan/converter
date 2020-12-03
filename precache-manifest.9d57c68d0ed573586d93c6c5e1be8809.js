14d3cd90e8ddcc5cca2541f68cf7d96af6313ee7
Daniil Davydov <dan0102dan@MacBook-Pro.local>
2020-12-03 09:57:37 +0300

    Updates

--- precache-manifest.9d57c68d0ed573586d93c6c5e1be8809.js
+++ precache-manifest.9d57c68d0ed573586d93c6c5e1be8809.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "db58b33dc753670a80549bbf48fcc702",
+    "revision": "ab49febe44323bfd2f136e2d4ad5b9f8",
     "url": "/converter/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/converter/static/css/2.1e562f28.chunk.css"
   },
   {
-    "revision": "7f8288a6946b5d13cf06",
+    "revision": "e6c92d8b43fed2aeba88",
     "url": "/converter/static/css/main.7a469cf5.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/converter/static/js/2.3b509cb8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7f8288a6946b5d13cf06",
-    "url": "/converter/static/js/main.b4abd9a3.chunk.js"
+    "revision": "e6c92d8b43fed2aeba88",
+    "url": "/converter/static/js/main.4e6736c9.chunk.js"
   },
   {
     "revision": "6235c77dd981c89dda52",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/converter/precache-manifest.ce428330157dba693d8ab2c7940fe740.js"
+  "/converter/precache-manifest.9d57c68d0ed573586d93c6c5e1be8809.js"
 );
 
 self.addEventListener('message', (event) => {
